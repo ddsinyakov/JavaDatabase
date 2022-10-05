0ad2e1e9bdaeed0e765161fe6e80b3fc4db3b23d
Дима Синяков <dimasinyakovwork@gmail.com>
2022-10-05 10:40:10 +0300

    Project setup to work with database

---
+++ src/main/java/step/learning/ConfigModule.java
@@ -0,0 +1,10 @@
+package step.learning;
+
+import com.google.inject.AbstractModule;
+
+public class ConfigModule extends AbstractModule {
+    @Override
+    protected void configure() {
+
+    }
+}
